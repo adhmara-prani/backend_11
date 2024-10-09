917c5e2d316cd9c0b0c15975ca3d730928b6d5a7
adhmara-prani <parth.saxena1021@gmail.com>
2024-10-09 09:00:41 +0530

    #

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const employeesController = require('../../controllers/employeesController');
 
-router.route('/')
-    .get(employeesController.getAllEmployees)
-    .post(employeesController.createNewEmployee)
-    .put(employeesController.updateEmployee)
-    .delete(employeesController.deleteEmployee);
+router
+  .route('/')
+  .get(employeesController.getAllEmployees) // handled like a waterfall since verifyJWT is a middleware
+  .post(employeesController.createNewEmployee)
+  .put(employeesController.updateEmployee)
+  .delete(employeesController.deleteEmployee);
 
-router.route('/:id')
-    .get(employeesController.getEmployee);
+router.route('/:id').get(employeesController.getEmployee);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
+const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 3500;
-
 // custom middleware logger
 app.use(logger);
 
@@ -16,8 +17,10 @@ app.use(cors(corsOptions));
 // built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
 
-// built-in middleware for json 
+// built-in middleware for json
 app.use(express.json());
+
+app.use(cookieParser());
 
 //serve static files
 app.use('/', express.static(path.join(__dirname, '/public')));
@@ -26,17 +29,19 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 app.use('/', require('./routes/root'));
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh'));
+app.use(verifyJWT);
 app.use('/employees', require('./routes/api/employees'));
 
 app.all('*', (req, res) => {
-    res.status(404);
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    } else if (req.accepts('json')) {
-        res.json({ "error": "404 Not Found" });
-    } else {
-        res.type('txt').send("404 Not Found");
-    }
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ error: '404 Not Found' });
+  } else {
+    res.type('txt').send('404 Not Found');
+  }
 });
 
 app.use(errorHandler);
